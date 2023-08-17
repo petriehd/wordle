1af1b26cdf0f234c32c8a07d87178bf337d7551e
James Petrie <jamesppetrie@hotmail.com>
2023-08-17 22:48:08 +1000

    Checks how many green and yellow tiles occur each turn

--- main.py
+++ main.py
@@ -11,27 +11,37 @@ wordList = GetWordList('wordList.txt').split('\n')
 
 def PrintBoard():
   # Clears terminal
-  os.system('cls' if os.name == 'nt' else 'clear')
+  # os.system('cls' if os.name == 'nt' else 'clear')
 
   for row in board:
     output = '|'
     for i in row: 
       output = output + i + '|' 
     print(output)
-    print("\033[0;37;42m Testing ")
 
 
-def CheckWord(word, row):
+def CheckWord(word, row, answer):
   # Check correct length
   if (len(word) != 5): 
     print('Word not of length 5!') 
     return False
-  
   # Check a valid word
   if (word not in guessList):
     print('Not a valid word!')
     return False
   
+  # Check against answer
+  # Need to update this to return these values or work on below
+  greens = 0
+  yellows = 0
+  for i in range(5):
+    if word[i] == answer[i]: 
+      greens += 1
+      continue
+    if word[i] in answer:
+      yellows += 1
+
+  print(greens, yellows)
 
   for i in range(5): board[row][i] = word[i]
   return True
@@ -45,6 +55,7 @@ def main():
 
   word = random.choice(wordList)
 
+
   row = 0
   win = False
   while (row < 6):
@@ -52,7 +63,7 @@ def main():
     if newWord == word: 
       win = True  
       break
-    if CheckWord(newWord, row):
+    if CheckWord(newWord, row, word):
       PrintBoard()
       row += 1
 
